ad982e940412e64c01ad171c6a3466e40d34caa9
Everson Coser <cosereverson@gmail.com>
2026-01-24 15:07:46 -0300

    Incremento de dto em loja

--- src/loja/loja.service.ts
+++ src/loja/loja.service.ts
@@ -1,15 +1,13 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateLojaDto } from './dto/create-loja.dto';
+import { UpdateLojaDto } from './dto/update-loja.dto';
 
 @Injectable()
 export class LojaService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(data: {
-    cnpj: string;
-    endereco: string;
-    telefone: string;
-  }) {
+  async create(data: CreateLojaDto) {
     return this.prisma.loja.create({
       data,
     });
@@ -19,28 +17,27 @@ export class LojaService {
     return this.prisma.loja.findMany();
   }
 
-    async findOne(idLoja: number) {
-        const loja = await this.prisma.loja.findUnique({
-            where: { idLoja },
-        });
-        if (!loja) {
-            throw new NotFoundException('Loja não encontrada');
-        }
-        return loja;
+  async findOne(idLoja: number) {
+    const loja = await this.prisma.loja.findUnique({
+      where: { idLoja },
+    });
+
+    if (!loja) {
+      throw new NotFoundException('Loja não encontrada');
     }
 
-    async update(
-        idLoja: number,
-        data: {
-            cnpj?: string;
-            endereco?: string;
-            telefone?: string;
-        },
-    ) {
-        await this.findOne(idLoja);
-        return this.prisma.loja.update({
-            where: { idLoja },
-            data,
-        });
-    }
+    return loja;
+  }
+
+  async update(
+    idLoja: number,
+    data: UpdateLojaDto,
+  ) {
+    await this.findOne(idLoja);
+
+    return this.prisma.loja.update({
+      where: { idLoja },
+      data,
+    });
+  }
 }
